eefafc999107357605b806b712ed6ad998e81118
AshbalAzam <ashbalazamrajpoot@gmail.com>
2026-07-14 18:16:43 +0500

    pdf reading and chunking done

---
+++ day 7/pdf_chunking.py
@@ -0,0 +1,13 @@
+from langchain_community.document_loaders import PyPDFLoader
+
+read = PyPDFLoader(file_path="day 7/test.pdf")
+pages = read.load_and_split()
+
+chunks= len(pages)
+print("number of chunks: ", chunks)
+# print the whole page
+print(pages[4])
+
+# give the first 100 charater
+# print(pages[0].page_content[:100]) 
+
